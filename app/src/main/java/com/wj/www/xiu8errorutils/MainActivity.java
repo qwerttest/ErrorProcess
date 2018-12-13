e5abff6f1684905e2771cac588be6e74f4ed58d2
qwerttest <1393235957@qq.com>
2018-12-13 12:13:37 +0800

    使用修改；

--- app/src/main/java/com/wj/www/xiu8errorutils/MainActivity.java
+++ app/src/main/java/com/wj/www/xiu8errorutils/MainActivity.java
@@ -15,41 +15,25 @@ public class MainActivity extends AppCompatActivity {
         initErrorProcess();
     }
 
-    private void initErrorProcess(){
+    private void initErrorProcess() {
         //默认处理
         ErrorUtils.getInstance().setDefaultProcess(new IErrorProcess() {
             @Override
-            public void error(int errorCode, String errorMsg) {
+            public void error(String errorCode, String errorMsg) {
             }
         });
-        //token失效
-        ErrorUtils.getInstance().registerErrorProcessDef(new IErrorProcess() {
-            @Override
-            public void error(int errorCode, String errorMsg) {
-            }
-        }, ServerCode.ERRORCODE_TOKEN_ILLEGAL, ServerCode.ERRORCODE_TOKEN_INVALID);
         //RefreshToken失效
         ErrorUtils.getInstance().registerErrorProcessDef(new IErrorProcess() {
-                                                             @Override
-                                                             public void error(int errorCode, String errorMsg) {
-                                                             }
-                                                         }, ServerCode.ERRORCODE_REFRESHTOKEN_ILLEGAL,
-                ServerCode.ERRORCODE_REFRESHTOKEN_INVALID,
-                ServerCode.ERRORCODE_REFRESHTOKEN_MISMATCH,
-                ServerCode.ERRORCODE_LOGIN_ONOTHERDEVICE);
+            @Override
+            public void error(String errorCode, String errorMsg) {
+            }
+        }, ServerCode.ERRORCODE_LOGIN_ONOTHERDEVICE);
         //余额不足统一处理
         ErrorUtils.getInstance().registerErrorProcessDef(new IErrorProcess() {
             @Override
-            public void error(int errorCode, String errorMsg) {
+            public void error(String errorCode, String errorMsg) {
 
             }
         }, ServerCode.ERRORCODE_BALANCE_NOTENOUGH);
-        //统一没有绑定手机提示实名认证,绑定手机dialog.
-        ErrorUtils.getInstance().registerErrorProcessDef(new IErrorProcess() {
-            @Override
-            public void error(int errorCode, String errorMsg) {
-
-            }
-        }, ServerCode.ERRORCODE_REAL_NAME);
     }
 }
--- app/src/main/java/com/wj/www/xiu8errorutils/ServerCode.java
+++ app/src/main/java/com/wj/www/xiu8errorutils/ServerCode.java
@@ -8,6 +8,7 @@ package com.wj.www.xiu8errorutils;
  * */
 public class ServerCode 
 {
-	public static final int ERRORCODE_BALANCE_NOTENOUGH 		= 123456;//余额不足
+	public static final String ERRORCODE_BALANCE_NOTENOUGH 		= "123456";//余额不足
 
+	public static final String ERRORCODE_LOGIN_ONOTHERDEVICE = "123695";
 }
